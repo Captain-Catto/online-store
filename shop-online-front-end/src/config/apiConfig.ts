27caef03250f936d01727c4a545ee743b6751315
Trí Đạt Lê Quang <lequangtridat2000@gmail.com>
2025-05-14 17:41:32 +0700

    sửa lỗi để truy cập backend

--- shop-online-front-end/src/config/apiConfig.ts
+++ shop-online-front-end/src/config/apiConfig.ts
@@ -1,3 +1,2 @@
 // Cấu hình URL cơ sở cho API
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
