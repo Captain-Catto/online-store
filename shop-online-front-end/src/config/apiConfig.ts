13f5e36d7cdd112c1a52d98d0e353c6492acc7b9
Trí Đạt Lê Quang <lequangtridat2000@gmail.com>
2025-05-14 13:54:01 +0700

    đổi env để deploy lên rail

--- shop-online-front-end/src/config/apiConfig.ts
+++ shop-online-front-end/src/config/apiConfig.ts
@@ -1,3 +1,3 @@
 // Cấu hình URL cơ sở cho API
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://0cb46962cf0d9d.lhr.life/api";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
