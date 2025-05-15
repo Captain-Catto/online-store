cc2cee8d7d13fd2b15ab2c7aad9d15b58743fd4e
Trí Đạt Lê Quang <lequangtridat2000@gmail.com>
2025-05-16 00:12:11 +0700

    chỉnh sửa lại icon

--- shop-online-front-end/src/components/Homepage/CasualProducts/CasualProducts-Banner.tsx
+++ shop-online-front-end/src/components/Homepage/CasualProducts/CasualProducts-Banner.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const CasualProductsBanner: React.FC = () => {
   return (
     <div className="relative w-full bg-gray-100 mt-4 overflow-hidden relative">
-      <div className="absolute top-1/3 left-[50px] text-center text-white z-10 flex flex-col lg:space-y-20 md:space-y-12 sm:space-y-6">
+      <div className="absolute top-1/3 left-[50px] text-center text-white z-8 flex flex-col lg:space-y-20 md:space-y-12 sm:space-y-6">
         <span className="text-2xl sm:text-4xl lg:text-6xl xl:text-7xl font-bold running-products-banner__title">
           ĐỒ THƯỜNG NGÀY
         </span>
